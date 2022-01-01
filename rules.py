a0f7c7ad440d44c6c2adb980161431ee9979eb0e
Kamil Bajorek <60983182+KamilBajorek@users.noreply.github.com>
2022-01-01 19:35:07 +0100

    rules wip

--- rules.py
+++ rules.py
@@ -1,3 +1,16 @@
 class RULES:
     def ktoWygral(self):
         raise NotImplementedError()
+
+
+class RULES_VERTICAL(RULES):
+    def ktoWygral(self):
+        pass
+
+class RULES_HORIZONTAL(RULES):
+    def ktoWygral(self):
+        pass
+
+class RULES_DIAGONALLY(RULES):
+    def ktoWygral(self):
+        pass
